5ae2c3c94151c3198a290e1514996842e995a2bf
radu129 <45313557+radu129@users.noreply.github.com>
2018-11-25 13:50:35 +0200

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,13 @@
+public class Data {
+    public static void main(String[] args) {
+        int myAge = 32;
+        float weight = 60.5f;
+        double height = 356.6;
+        long distance = 1000356832555l;
+        char gender = 'm';
+        char test = 400;
+        boolean isStudent = false;
+        boolean hasJob = true;
+        System.out.print(gender);
+    }
+}
